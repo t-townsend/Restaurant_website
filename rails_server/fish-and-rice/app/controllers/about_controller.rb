bceb765cfc50b3de82f731081943a91330fdf87e
Trista Townsend <tristatownsend@gmail.com>
2017-07-24 12:58:37 -0700

    added rails controllers and routes

---
+++ rails_server/fish-and-rice/app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+class AboutController < ApplicationController
+
+  def new
+  end
+  
+end
--- rails_server/fish-and-rice/config/routes.rb
+++ rails_server/fish-and-rice/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get('/about', { to: 'about#index' })
+
+  get('/contact_us', { to: 'contact#new', as: 'contact' })
+
+  get('/menu', { to: 'menu#new' })
+
+   root 'welcome#index'
 end
